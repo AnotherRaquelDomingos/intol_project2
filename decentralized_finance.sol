eeb5fa8a33c4a6e6e620163e7d920d282c97b1e4
fc56378 <90569668+AnotherRaquelDomingos@users.noreply.github.com>
2024-05-27 09:39:00 +0100

    inital commit

---
+++ decentralized_finance.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract DecentralizedFinance is ERC20 {
+    // TODO: define variables
+    using Counters for Counters.Counter;
+    struct Loan {
+        uint256 deadline;
+        uint256 amount;
+        address lender;
+        address borrower;
+        bool isBasedNft;
+        bool nftContract;
+        bool nftId;
+    }
+    address owner;
+    uint256 maxLoanDuration;
+    uint256 dexSwapRate;
+    uint256 balance;
+    mapping(Counters.Counter => Loan) loans;
+
+    
+    event loanCreated(address indexed borrower, uint256 amount, uint256 deadline);
+
+    constructor() ERC20("DEX", "DEX") {
+        owner = msg.sender;
+        _mint(address(this), 10**18);
+        // TODO: initialize
+    }
+
+    function buyDex() external payable {
+        require(msg.value >= dexSwapRate, "The value is not enough to buy one DEX.");
+        uint256 quantityDEX = msg.value / dexSwapRate;
+        _mint(msg.sender, quantityDEX);
+        balance += quantityDEX * dexSwapRate;
+    }
+
+    function sellDex(uint256 dexAmount) external {
+        // TODO: implement this
+    }
+
+    function loan(uint256 dexAmount, uint256 deadline) external {
+        // TODO: implement this
+
+        emit loanCreated(msg.sender, loanAmount, deadline);
+    }
+
+    function returnLoan(uint256 ethAmount) external {
+        // TODO: implement this
+    }
+
+    function getBalance() public view returns (uint256) {
+        require(msg.sender == owner, "Only the owner of the contract can observe the balance.");
+        return balance;
+    }
+
+    function setDexSwapRate(uint256 rate) external {
+        require(msg.sender == owner, "Only the owner of the contract can change the dex swap rate.");
+        dexSwapRate = rate;
+    }
+
+    function getDexBalance() public view returns (uint256) {
+        // TODO: implement this
+    }
+
+    function makeLoanRequestByNft(IERC721 nftContract, uint256 nftId, uint256 loanAmount, uint256 deadline) external {
+        // TODO: implement this
+    }
+
+    function cancelLoanRequestByNft(IERC721 nftContract, uint256 nftId) external {
+        // TODO: implement this
+    }
+
+    function loanByNft(IERC721 nftContract, uint256 nftId) external {
+        // TODO: implement this
+
+        emit loanCreated(msg.sender, loanAmount, deadline);
+    }
+
+    function checkLoan(uint256 loanId) external {
+        // TODO: implement this
+    }
+}
